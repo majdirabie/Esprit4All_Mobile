8404d722cd2c10e58ef4bdf2c147de35abce53d4
sami <sami.marrekchi@esprit.tn>
2017-09-30 16:59:01 +0100

    update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("tegGit");
+        primaryStage.setTitle("majdiGit");
+        System.out.println("je suis le github");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
